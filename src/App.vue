74bb6d6b60c2a4d50bc731e2cc5825e02186a880
Geardo Astro <ralukoe@gmail.com>
2024-01-29 17:12:39 -0700

    Pokemons Router Vue3

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+
+<nav class="navbar bg-body-tertiary">
+  <div class="container">
+    <router-link class="navbar-brand" to="/">
+      <img src="@/assets/logo.svg" alt="Bootstrap" width="30" height="24">
+      PokeAPI
+    </router-link>
+  </div>
+  <router-link  class="btn btn-outline primary" to="/">Home</router-link>
+  <router-link class="btn btn-outline primary" to="/pokemons">Pokemons</router-link>
+</nav>
+
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/pokemons">Pokemons</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+  <div class="container"> </div>
+  <RouterView />
+  <footer>Footer</footer>
+</template>
+
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+
+<template>
+    <h1>404</h1>
+    <router-link  class="btn btn-outline primary" to="/">Volver a Home</router-link>
+</template>
